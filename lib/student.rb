d6f765438ab8c62860ee16fec1e780d0bc62642e
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-06 03:03:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,6 +29,25 @@ class Student
     DB[:conn].execute("DROP TABLE students")
   end
 
+
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.name = name
+    student.grade = grade
+    student.save
+  end
+
+  def self.new_from_db(row)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.create(row[1],row[2],row[0])
+    end
+  end
+
   def save
     if self.id
       self.update
@@ -41,13 +60,6 @@ class Student
       DB[:conn].execute(sql, self.id, self.name, self.grade)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
-  end
-
-  def self.create(name, grade)
-    student = Student.new(name, grade)
-    student.name = name
-    student.grade = grade
-    student.save
   end
 
   def update
