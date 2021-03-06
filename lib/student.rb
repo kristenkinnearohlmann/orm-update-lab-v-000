2a2ddafd52218fe5fb27e827a907161b7d927e1c
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-06 02:55:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,7 +45,7 @@ class Student
 
   def update
       sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
-      DB[:conn].execute(sql, self.name,. self.grade, self.id)
+      DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
 end
