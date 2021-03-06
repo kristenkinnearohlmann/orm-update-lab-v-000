83041a882d1c53b378b6abfd16f8bd5dfcbabff2
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-06 02:53:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,7 +40,7 @@ class Student
 
       DB[:conn].execute(sql, self.id, self.name, self.grade)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-    wns
+    end
   end
 
 end
